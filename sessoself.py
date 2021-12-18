3edca7f9721d33b9ff887f37da31b6b881b76e90
Komodo <68278515+Lojacops@users.noreply.github.com>
2021-12-18 22:09:16 +0100

    Update sessoself.py

--- sessoself.py
+++ sessoself.py
@@ -1,1 +1,20 @@
-#neg-
+import discord
+from discord.ext import commands
+
+print("""
+
+logo
+
+
+
+""")
+
+
+token = input("token: ")
+
+science = commands.Bot(command_prefix="-", self_bot=True)
+
+@science.event
+async def on_ready():
+  print("ciao mk")
+ 
